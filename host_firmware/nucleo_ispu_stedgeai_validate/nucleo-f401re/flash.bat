6eb5ead57da070d97a940c4c450af6ce570aab6f
Lorenzo Bracco <lorenzo.bracco@st.com>
2025-01-27 11:31:53 +0100

    stedgeai fw: add flash scripts
    Signed-off-by: Lorenzo Bracco <lorenzo.bracco@st.com>

---
+++ host_firmware/nucleo_ispu_stedgeai_validate/nucleo-f401re/flash.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set script_dir=%~dp0
+
+set programmer=STM32_Programmer_CLI.exe
+
+where /q %programmer%
+if %errorlevel% neq 0 (
+	echo Cannot find STM32_Programmer_CLI.exe command
+	exit /b 1
+)
+
+if "%2" neq "" (
+	echo Usage: flash.bat [probe_index]
+	exit /b 2
+)
+
+for /f %%l in ('%programmer% --list ^| find "ST-Link Probe"') do (
+	set /a num_probes+=1
+)
+
+set probe=0
+if "%1" neq "" (
+	set probe=%1
+) else if %num_probes% gtr 1 (
+	echo More than one ST-Link probe detected, please specify the index of the probe to use.
+	echo The available probes can be listed with: %programmer% --list.
+	echo Usage: flash.bat ^<probe_index^>
+	exit /b 3
+)
+
+%programmer% -c port=SWD index=%probe% -w "%script_dir%\build\nucleo_ispu_stedgeai_validate.bin" 0x8000000 -s 0x8000000
+
